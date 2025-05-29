73bb2c797b37894f7ab46ddf737fbfad295c558e
Dinne Kopelevich <dinne.kopelevich@gsa.gov>
2025-05-29 08:21:32 -0600

    Remove comments, add lang tag, title tag and meta tag
    Signed-off-by: Dinne Kopelevich <dinne.kopelevich@gsa.gov>

--- js/formActions.js
+++ js/formActions.js
@@ -24,7 +24,7 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         } 
 
-        emailFile(window.lastSubmission)
+        emailFile(window.lastSubmission);
     });
 });
 
